a86fac8e7bbbe3bcc272ba097ce8619e4eafadd6
alphatrex44 <1815dc@vanguardcharter.org>
2025-09-16 09:11:52 -0600

    fixed it (i hope)

--- Programming Assignments/Assignment_2.py
+++ Programming Assignments/Assignment_2.py
@@ -63,28 +63,49 @@ def next_prime(number: int) -> int:
         return 2
 
 def this_many_primes(number: int) -> list:
-    known_primes = make_prime_list(number*number)
+    if number <= 0:
+        return []
+
+    known_primes = make_prime_list(max(2, number * number))  # ✅ ensures input is at least 2
     prime_count = []
-    if number >= 2:
-        for prime in known_primes:
-            if len(prime_count) < number:
-                prime_count.append(prime)
-            else:
-                return prime_count
-    else:
-        return "None"
-def main() -> None:
-    while True:
-        try:
-            number = int(input("Give a number please: "))
+
+    for prime in known_primes:
+        if len(prime_count) < number:
+            prime_count.append(prime)
+        else:
             break
-        except ValueError:
-            print("Not a valid number!")
-    print(odd_or_even(number))
-    print(count_the_digits(number))
-    print(is_prime(number))
-    print(next_prime(number))
-    print(make_prime_list(number))
-    print(this_many_primes(number))
+
+    return prime_count
+
+def main() -> None:
+    print("Welcome to my calculator.")
+    while True:
+        while True:
+            option = input("If you would like to determine even or odd, press 1 \nIf you would like to count the digits, press 2 \nIf you would like a list of primes up to your number, press 3 \nIf you would like to determine whether your number is a prime, press 4 \nIf you would like to find the prime after or including your number, press 5 \nIf you would like that amount of primes press 6 \n")
+            if option == "1" or option == "2" or option == "3" or option == "4" or option == "5" or option == "6" or option == "":
+                break
+            else:
+                print("Not a valid option!")
+        if option == "":
+            break
+        while True:
+            try:
+                number = int(input("Give a number please for your operation: "))
+                break
+            except ValueError:
+                print("Not a valid number!")
+        if option == "1":
+            print(odd_or_even(number))
+        if option == "2":
+            print(count_the_digits(number))
+        if option == "3":
+            print(is_prime(number))
+        if option == "4":
+            print(next_prime(number))
+        if option == "5":
+            print(make_prime_list(number))
+        if option == "6":
+            print(this_many_primes(number))
+
 if __name__ == "__main__":
     main()
